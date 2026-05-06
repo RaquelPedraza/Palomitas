c6c272655e5da2697a90c61df00beeeaf9d75099
Raquel Pedraza <raquelpt27@gmail.com>
2026-05-06 17:45:05 +0200

    style: aplicados estilos flex al navbar

--- index.php
+++ index.php
@@ -145,33 +145,37 @@ if ($id_usuario) {
 <head>
     <meta charset="UTF-8">
     <title>Palomitas | Pág <?= $pagina_actual ?></title>
-    <link rel="stylesheet" href= "css/estilos.css?v=6"></link>
+    <link rel="stylesheet" href= "css/estilos.css?v=7"></link>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
 </head>
 <body>
     <nav class="navbar">
-    <a href="index.php" class="logo" style="display: flex; align-items: center; text-decoration: none;">
-        <img src="img/logo.png" alt="Logo Palomitas" style="height: 50px; margin-right: 10px;">
-    </a>
-    
-    <div class="enlaces">
-            <a href="index.php">Catálogo</a>
+        <div class="nav-izq" style="display: flex; align-items: center; gap: 20px;">
+            <a href="index.php" class="logo" style="display: flex; align-items: center; text-decoration: none;">
+                <img src="img/logo.png" alt="Logo Palomitas" style="height: 50px; margin-right: 10px;">
+            </a>
+            <div class="enlaces">
+                <a href="index.php">Catálogo</a>
+                <?php if (isset($_SESSION['id_usuario'])): ?> 
+                    <a href="mis_listas.php">Mis listas</a>
+                <?php endif; ?>
+            </div>
+        </div> 
 
-            <?php if (isset($_SESSION['usuario_id'])): ?>
-                <a href="mis_listas.php">Mis listas</a>
-            <?php endif; ?>
+        <div class="enlaces-usuario">    
             <?php if (isset($_SESSION['usuario_nombre'])): ?>
-                <span style="color: #ccc; margin-left: 20px;">
+                <span style="color: #ccc; font-size: 1.1em; margin-left: 20px;">
                     Hola, <strong style="color: white;"><?= htmlspecialchars($_SESSION['usuario_nombre']) ?></strong>
                 </span>
-                <a href="logout.php" style="color: #e50914; margin-left: 15px;">Cerrar Sesión</a>
+                <a href="logout.php" style="color: #e50914; font-size: 1.1em; margin-left: 15px;">Cerrar Sesión</a>
             <?php else: ?>
                 <a href="login.php">Iniciar Sesión</a>
                 <a href="registro.php">Crear Cuenta</a>
             <?php endif; ?>
         </div>
     </nav>
+                   
 
    <!-- TÍTULO PRINCIPAL --> 
     <img class="logo-principal" src="img/icono.png" alt="Icono Palomitas">
